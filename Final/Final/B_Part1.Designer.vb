c2189f4683d19505a28b15b991e296fcb291407a
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-12-14 10:59:03 -0600

    update

--- Final/Final/B_Part1.Designer.vb
+++ Final/Final/B_Part1.Designer.vb
@@ -22,16 +22,29 @@ Partial Class B_Part1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(928, 406)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
         '
         'B_Part1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(925, 400)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "B_Part1"
         Me.Text = "B_Part1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
 End Class
