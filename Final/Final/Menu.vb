5f88bd5bcc2f1ec5a00c83f135c9c20192489362
Andrew <heldiggris7@gmail.com>
2017-12-14 10:27:22 -0600

    update Menu
    Andrew

--- Final/Final/Menu.vb
+++ Final/Final/Menu.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Menu
 
+    Dim second As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Interval = 1000
+        Timer1.Start() 'Timer starts functioning
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        picLogo.Visible = False
+        lblLogoName.Visible = False
+
+        second = second + 1
+        If second >= 2 Then
+            btnStart.Visible = True
+            btnExit.Visible = True
+        End If
+
+    End Sub
+
+
 End Class
+
