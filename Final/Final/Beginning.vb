2c912e40895b1659852bb58511f1eb53b1c352d4
Andrew <heldiggris7@gmail.com>
2017-12-15 10:27:28 -0600

    update ChangeGem

--- Final/Final/Beginning.vb
+++ Final/Final/Beginning.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Beginning
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
         Me.Close()
         MainMenu.Visible = True
     End Sub
