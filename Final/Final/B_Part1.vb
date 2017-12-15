cce52d962e03ea48f46ae87153d3cd92a775d76c
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-12-15 11:13:50 -0600

    update level one part1/2

--- Final/Final/B_Part1.vb
+++ Final/Final/B_Part1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class B_Part1
 
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
--- Final/Final/My Project/Resources.Designer.vb
+++ Final/Final/My Project/Resources.Designer.vb
@@ -63,6 +63,26 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property plains_level() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("plains level", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property platform() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("platform", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property village() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("village", resourceCulture)
